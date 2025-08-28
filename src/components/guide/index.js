843ac9d65c4122986d548d8955f12d8540dad5df
hfub0310077 <hfub0310077@gmail.com>
2025-08-29 00:42:06 +0800

    Update index.js

--- src/components/guide/index.js
+++ src/components/guide/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import QRCode from 'qrcode';
 import style from './index.less';
-import { transform, i18n, lan } from '../../unit/const';
+import { transform } from '../../unit/const';
 import { isMobile } from '../../unit';
 
 export default class Guide extends React.Component {
@@ -9,19 +8,7 @@ export default class Guide extends React.Component {
     super();
     this.state = {
       isMobile: isMobile(),
-      QRCode: '',
     };
-  }
-
-  componentWillMount() {
-    if (this.state.isMobile) return;
-
-    QRCode.toDataURL(location.href, { margin: 1 })
-      .then(dataUrl => this.setState({ QRCode: dataUrl }));
-  }
-
-  shouldComponentUpdate(state) {
-    return state.QRCode !== this.state.QRCode;
   }
 
   render() {
@@ -44,21 +31,10 @@ export default class Guide extends React.Component {
             <em style={{ [transform]: 'translate(7px,3px)rotate(90deg) scale(1,2)' }} />
           </div>
         </div>
-
         {/* 左側提示文字 */}
         <div className={`${style.guide} ${style.left}`}>
           <div className={style.space}>SPACE</div>
         </div>
-
-        {/* QR Code */}
-        {this.state.QRCode && (
-          <div className={`${style.guide} ${style.qr}`}>
-            <img
-              src={this.state.QRCode}
-              alt={i18n.QRCode[lan]}
-            />
-          </div>
-        )}
       </div>
     );
   }
